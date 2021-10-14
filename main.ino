fa1db1dd738889b8e28528446c827c2b1373c374
NekoRectifier <hyx123250@gmail.com>
2021-10-14 20:39:36 +0800

    修改了之前的错误

--- main.ino
+++ main.ino
@@ -1,26 +1,34 @@
 #include <Adafruit_NeoPixel.h>
+
+using namespace std;
+
 #ifdef __AVR__
     #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
 #endif
 
+//consants definitions
 #define LED_COUNT 8
+#define USS_CENTER_OFFSET 0.0 //TODO: Needs to be modified when vehicle is built up.
 
 //pin set
 #define LED_pin 6
 #define TRIG_pin 13
 #define ECHO_pin 11
-#define CRASH_BTN 10
+#define CRASH_pin 10
 
 //global varible init.
 volatile double initialDistance = 0.0;
-Adafruit_NeoPixel strip(LED_COUNT, LED_pin, NEO_GRB, NEO_KHZ800);
+int CRASH_BTN_STATE = 1;
+Adafruit_NeoPixel strip(LED_COUNT, LED_pin, NEO_GRB + NEO_KHZ800);
 
 //function pre-declare.
 void initComponents();
 void initNeoPixel();
 void initUltraSonic();
-void flash(int pattern);
-
+void colorWipe(uint32_t color, int wait);
+void flash(uint32_t color,int perTime, int times);
+double getDistance();
+void beginTaskSequence();
 
 //main function.
 void setup()
@@ -29,32 +37,60 @@ void setup()
     delay(100);
     initComponents();
 
-    flash(0);
-    
+    strip.clear();
+    flash(strip.Color(255,100,100),200,2);
+    delay(100);
+    beginTaskSequence();
 }
 
 void loop()
 {
-
+    //Serial.println(getDistance());
 }
 
 
 //main body of sub_functions
 void initComponents() 
 {
-    flash(10000);
-    initUltraSonic();
     initNeoPixel();
+    //flash(strip.Color(128,128,0),150,4);
+    initUltraSonic();
+    pinMode(CRASH_pin, INPUT);
+    //TODO motors need to be initialized...
 }
 
 void initUltraSonic()
 {
-    pinMode(2, OUTPUT);
-    pinMode(3, INPUT);
+    pinMode(13, OUTPUT);
+    pinMode(11, INPUT);
     delay(200);
-    initialDistance =  getDistance();
     //TODO: Show different light pattern correspond to each task
-    Serial.println(initialDistance);
+    Serial.println(getDistance());
+    delay(700);
+    Serial.println(getDistance());
+    delay(700);
+    Serial.println(getDistance());
+    delay(700);
+    Serial.println(getDistance());
+    delay(700);
+    Serial.println(getDistance());
+    delay(700);
+    Serial.println(getDistance());
+    delay(700);
+    Serial.println(getDistance());
+    delay(700);
+    Serial.println(getDistance());
+    delay(700);
+    Serial.println(getDistance());
+    delay(700);
+    Serial.println(getDistance());
+    delay(700);
+    Serial.println(getDistance());
+    delay(700);
+    Serial.println(getDistance());
+    delay(700);
+    Serial.println(getDistance());
+    
 }
 
 void initNeoPixel()
@@ -65,35 +101,18 @@ void initNeoPixel()
     #endif
 
     strip.begin();
-    strip.stop();   //Nya?
+    strip.clear();   //Nya?
 
     //strip.setBrightness(255);
 
     colorWipe(strip.Color(127,127,127),50);
     delay(5000);
-    strip.stop();
+    strip.clear();
 
-}
-
-void flash(int pattern)
-{
-    switch(i)
-    {
-        case 0: //Car is ready
-            flash(strip.Color(0,128,0),200,2,{0,1,6,7});
-            break;
-        case -1:
-            flash(strip.Color(255,0,0),1000,3,{0,2,5,7});
-            break;
-        case 10000:
-            break;
-        default:
-            break;
-    }
 }
 
 //NeoPixel custom functions
-void colorWipe(uint32_t color, int wait) 
+void colorWipe(uint32_t color, int wait)
 {
     for(int i=0; i<strip.numPixels(); i++)
     {
@@ -103,18 +122,20 @@ void colorWipe(uint32_t color, int wait)
     }
 }
 
-void flash(uint32_t color,int perTime, int times, int[] pixels)
+void flash(uint32_t color,int perTime, int times)
 {
+
     for(int j = 0; j < times; j++)
     {
-        for(int& i : pixels)
-        {
-            strip.setPixelColor(i,color);
-        }
+        strip.setPixelColor(0, color);
+        strip.setPixelColor(1, color);
+        strip.setPixelColor(6, color);
+        strip.setPixelColor(7, color);
         strip.show();
         delay(perTime);
         strip.clear();
     }
+    strip.clear();
     
 }
 
@@ -123,12 +144,32 @@ double getDistance()
 
     //Origin merchant instance code, NOT modified yet.
 
-    digitalWrite(2, LOW);
-    delayMicroseconds(2);
-    digitalWrite(2, HIGH);
+    digitalWrite(13, LOW);
+    delayMicroseconds(13);
+    digitalWrite(13, HIGH);
     delayMicroseconds(10);
-    digitalWrite(2, LOW);
-    double distance = pulseIn(3, HIGH) / 58.00;
+    digitalWrite(13, LOW);
+    double distance = pulseIn(11, HIGH) / 58.00;
     delay(10);
     return distance;
+    //Outputs maybe in centimeter.
+}
+
+void beginTaskSequence()
+{
+    //if ultrasonic module is fine...
+    initialDistance = getDistance();
+    double total = initialDistance + USS_CENTER_OFFSET;
+
+    if (total > 320.0 && total < 400.0) {
+        flash(strip.Color(255,255,255),100,2);
+    } else if(total > 240.0 && total < 320.0) {
+        flash(strip.Color(255,255,255),100,2);
+    } else if(total > 140.0 && total < 240.0) {
+        flash(strip.Color(255,255,255),100,2);
+    } else {
+    }
+
+    //motor.start();
+    
 }
