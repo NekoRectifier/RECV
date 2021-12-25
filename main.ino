54577194d4f7587a072fd80d99e6528559696832
NekoRectifier <hyx123250@gmail.com>
2021-12-25 21:26:18 +0800

    未测验过的代码 - 添加碰撞检测相关

--- main.ino
+++ main.ino
@@ -7,6 +7,7 @@ using namespace std;
 #define PWMA 11  //占空比电压调制
 #define ENCODER_A 2
 #define ENCODER_B 3
+#define CRASH_BTN [?]
 
 #define LED 5
 #define LED_COUNT 8
@@ -16,8 +17,10 @@ Adafruit_NeoPixel strip(LED_COUNT, LED, NEO_GRB + NEO_KHZ800);
 
 volatile long encoder_A;
 volatile long encoder_B;
+volatile int flag_crash = 0;
 
 unsigned long curr_time, prev_time = 0;
+long odometer = 0;
 
 double velocity = 0.0;
 
@@ -36,6 +39,7 @@ void setup()
 
     pinMode(ENCODER_A, INPUT);
     pinMode(ENCODER_B, INPUT);
+    pinMode(CRASH_BTN, INPUT);
 
     Serial.begin(9600);
     //initLED();
@@ -46,7 +50,10 @@ void setup()
     prev_time = millis();
 
     attachInterrupt(0, ISR_enc_A, CHANGE);  //开始中断 函数:timer 模式:RISING
-    attachInterrupt(1, ISR_enc_B, CHANGE);
+    //attachInterrupt(1, ISR_enc_B, CHANGE);
+    attachInterrupt(1), crashDetect, CHANGE);
+
+    // 由于Arduino有且仅有两个中断针脚, 故只采用编码器的A段信息输入
 
     analogWrite(PWMA, pwm);  //给定pwm模拟输出
 
@@ -57,13 +64,23 @@ void setup()
 void loop()
 {
     noInterrupts();
-    Serial.print("ENC_LEFT: ");
-    Serial.println(readEncoderValue(0));
+    // Serial.print("ENC_LEFT: ");
+    // Serial.println(readEncoderValue(0));
 
-    Serial.print("ENC_RIGHT: ");
-    Serial.println(readEncoderValue(1));
+    // Serial.print("ENC_RIGHT: ");
+    // Serial.println(readEncoderValue(1));
 
-    lapRecognition();
+    if (flag_crash == 1)
+    {
+        flag_crash == 2;
+        odometer = encoder_A;
+        encoder_A = 0;
+        digitalWrite(MOTOR_1, HIGH);
+        digitalWrite(MOTOR_2, LOW);
+    }
+
+    mileageCheck();
+
     interrupts();
 }
 
@@ -89,65 +106,46 @@ void ISR_enc_A()
     encoder_A++;
 }
 
-void ISR_enc_B()
+// void ISR_enc_B()
+// {
+//     encoder_B++;
+// }
+
+void crashDetect()
 {
-    encoder_B++;
+    flag_crash = 1;
 }
 
-long readEncoderValue(int seletor)
+void mileageCheck()
 {
     noInterrupts();
-    long curr;
-
-    if (seletor == 0)
+    if (flag_crash = 2)
     {
-        curr = encoder_A;
+        if (encoder_A >= odometer) {
+            digitalWrite(MOTOR_1, LOW);
+            digitalWrite(MOTOR_2, LOW);
+        }
     }
-    else
-    {
-        curr = encoder_B;
-    }
-    interrupts();
-    return curr;
-}
-
-void lapRecognition()
-{
-    noInterrupts();
-
-    if (encoder_A >= TOTAL_PULSE_WHELL)
-    {
-        encoder_A = 0;
-        //        digitalWrite(MOTOR_1, LOW);
-        //        digitalWrite(MOTOR_2, LOW);
-    }
-    else if (encoder_B >= TOTAL_PULSE_WHELL)
-    {
-        //        digitalWrite(MOTOR_1, LOW);
-        //        digitalWrite(MOTOR_2, LOW);
-        encoder_B = 0;
-    }
-
     interrupts();
 }
 
-bool speedMeasure()  //for high speed
-{
-    curr_time = millis();
-    noInterrupts();
+// bool speedMeasure()  //for high speed
+// {
+//     curr_time = millis();
+//     noInterrupts();
 
-    unsigned long total_encode = encoder_A + encoder_B;
+//     unsigned long total_encode = encoder_A + encoder_B;
 
-    if (abs(curr_time - prev_time) >= 100)
-    {
-        velocity = total_encode / (1560 * 0.1);
+//     if (abs(curr_time - prev_time) >= 100)
+//     {
+//         velocity = total_encode / (1560 * 0.1);
 
-        encoder_A = encoder_B = 0;
-        return true;
-    }
-    else
-    {
-        prev_time = curr_time;
-    }
-    interrupts();
-}
+//         encoder_A = encoder_B = 0;
+//         return true;
+//     }
+//     else
+//     {
+//         prev_time = curr_time;
+//     }
+//     interrupts();
+// }
