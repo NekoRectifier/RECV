283f5bc0bcaf6645a1275bc440ba94a00293cb80
NekoRectifier <hyx123250@gmail.com>
2021-12-30 21:02:07 +0800

    Update main.ino
    1. 更改适配从'Uno'到'Leonardo'
    2. 成功测试碰撞传感器的功能

--- main.ino
+++ main.ino
@@ -2,60 +2,73 @@
 
 using namespace std;
 
-#define MOTOR_1 9
-#define MOTOR_2 10
-#define PWMA 11  //占空比电压调制
+// 针脚定义 开始
+#define MOTOR_A_POS 9
+#define MOTOR_A_NEG 10
+#define MOTOR_B_POS 9
+#define MOTOR_B_NEG 10
 #define ENCODER_A 2
 #define ENCODER_B 3
+#define ENCODER_C 
+#define ENCODER_D 
 #define CRASH_BTN [?]
-
 #define LED 5
 #define LED_COUNT 8
+//占空比电压调制
+#define PWMA 11  
+#define PWMB 11
+//针脚定义结束
+
 Adafruit_NeoPixel strip(LED_COUNT, LED, NEO_GRB + NEO_KHZ800);
 
 #define TOTAL_PULSE_WHELL 1560
 
-volatile long encoder_A;
-volatile long encoder_B;
+volatile long encoder_A = 0;
+volatile long encoder_B = 0;
+volatile long encoder_C = 0;
+volatile long encoder_D = 0;
 volatile int flag_crash = 0;
-
 unsigned long curr_time, prev_time = 0;
 long odometer = 0;
-
 double velocity = 0.0;
-
-int pwm = 75;  //固定为该数值 正好
+int pwm = 75;
 
 // 中断函数内所有的变量在声明时都应该加上volatile属性
-// Arduino Uno 仅支持2,3针脚作为中断引脚
 
 void setup()
 {
-    pinMode(MOTOR_1, OUTPUT);
-    pinMode(MOTOR_2, OUTPUT);
+    pinMode(MOTOR_A_POS, OUTPUT);
+    pinMode(MOTOR_A_NEG, OUTPUT);
     pinMode(PWMA, OUTPUT);
-
-    //pinMode(LED, OUTPUT);
-
+    pinMode(MOTOR_B_POS, OUTPUT);
+    pinMode(MOTOR_B_NEG, OUTPUT);
+    pinMode(PWMB, OUTPUT);
     pinMode(ENCODER_A, INPUT);
     pinMode(ENCODER_B, INPUT);
+    pinMode(ENCODER_C, INPUT);
+    pinMode(ENCODER_D, INPUT);
     pinMode(CRASH_BTN, INPUT);
+    pinMode(LED_BULITIN, OUTPUT);
 
     Serial.begin(9600);
-    //initLED();
 
-    digitalWrite(MOTOR_1, LOW);
-    digitalWrite(MOTOR_2, HIGH);
+    digitalWrite(LED_BUILTIN, LOW);
+    digitalWrite(MOTOR_A_POS, LOW);
+    digitalWrite(MOTOR_A_NEG, HIGH);
+    digitalWrite(MOTOR_B_POS, LOW);
+    digitalWrite(MOTOR_B_NEG, HIGH);
 
     prev_time = millis();
 
-    attachInterrupt(0, ISR_enc_A, CHANGE);  //开始中断 函数:timer 模式:RISING
-    //attachInterrupt(1, ISR_enc_B, CHANGE);
-    attachInterrupt(1), crashDetect, CHANGE);
-
-    // 由于Arduino有且仅有两个中断针脚, 故只采用编码器的A段信息输入
+    attachInterrupt(digitalPinToInterrupt(0), ISR_enc_A, CHANGE);  //开始中断 函数:timer 模式:RISING
+    attachInterrupt(digitalPinToInterrupt(1), ISR_enc_B, CHANGE);
+    //attachInterrupt(digitalPinToInterrupt(pin), function, mode);
+    //attachInterrupt(digitalPinToInterrupt(pin), function, mode);
+    
+    attachInterrupt(digitalPinToInterrupt(7), crashDetect, CHANGE);
 
     analogWrite(PWMA, pwm);  //给定pwm模拟输出
+    analogWrite(PWMB, pwm);
 
     interrupts();
     // RISING 指在引脚由低电平转为高电平时触发
@@ -64,19 +77,16 @@ void setup()
 void loop()
 {
     noInterrupts();
-    // Serial.print("ENC_LEFT: ");
-    // Serial.println(readEncoderValue(0));
-
-    // Serial.print("ENC_RIGHT: ");
-    // Serial.println(readEncoderValue(1));
 
     if (flag_crash == 1)
     {
-        flag_crash == 2;
+        flag_crash == 2;                    //代表已处理
+        digitalWrite(LED_BUILTIN, HIGH);
         odometer = encoder_A;
+        //TODO: 此处应对编码器数值做精准化处理
         encoder_A = 0;
-        digitalWrite(MOTOR_1, HIGH);
-        digitalWrite(MOTOR_2, LOW);
+        digitalWrite(MOTOR_A_POS, HIGH);
+        digitalWrite(MOTOR_A_NEG, LOW);
     }
 
     mileageCheck();
@@ -84,46 +94,39 @@ void loop()
     interrupts();
 }
 
-void colorWipe(uint32_t color, int wait)
-{
-    for (int i = 0; i < strip.numPixels(); i++)
-    {
-        strip.setPixelColor(i, color);
-        strip.show();
-        delay(wait);
-    }
-}
-
-void initLED()
-{
-    strip.begin();
-    colorWipe(strip.Color(255, 255, 24), 5000);
-    strip.clear();
-}
-
 void ISR_enc_A()
 {
     encoder_A++;
 }
 
-// void ISR_enc_B()
-// {
-//     encoder_B++;
-// }
+void ISR_enc_B()
+{
+    encoder_B++;
+}
+
+void ISR_enc_C()
+{
+    encoder_C++;
+}
+
+void ISR_enc_D()
+{
+    encoder_D++;
+}
 
 void crashDetect()
 {
     flag_crash = 1;
 }
 
-void mileageCheck()
+void mileageCheck()                         //用于控制车辆在终点处停下
 {
     noInterrupts();
     if (flag_crash = 2)
     {
         if (encoder_A >= odometer) {
-            digitalWrite(MOTOR_1, LOW);
-            digitalWrite(MOTOR_2, LOW);
+            digitalWrite(MOTOR_A_POS, LOW);
+            digitalWrite(MOTOR_A_NEG, LOW);
         }
     }
     interrupts();
