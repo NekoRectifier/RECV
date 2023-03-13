9a167ae2d71ae94050b22d0c90ebe1ad514ba66b
NekoRectifier <hyx123250@gmail.com>
2023-03-13 23:34:15 +0800

    feat: can control aux bulitin led and fix bilnk

--- RECV.ino
+++ RECV.ino
@@ -8,7 +8,8 @@ ESP8266WebServer server(80);
 //TODO may change server port to 443 soon
 
 // pin mode definition
-uint8 B_LED = LED_BUILTIN;
+uint8 A_LED = LED_BUILTIN;
+uint8 B_LED = LED_BUILTIN_AUX;
 uint8 PWM = D2;
 uint8 IN2 = D3;
 uint8 IN1 = D4;
@@ -17,21 +18,27 @@ uint8 ENC_A = D6;
 uint8 ENC_B = D5;
 
 void setup() {
+    // supress led from enabling
+    digitalWrite(A_LED, HIGH); // high means off (lights)
     digitalWrite(B_LED, HIGH);
 
     Serial.begin(9600);
 
     // initiate pin mode
+    pinMode(A_LED, OUTPUT); // BUILTIN LED
     pinMode(B_LED, OUTPUT); // BUILTIN LED
 
     WiFi.softAP(ssid, passwd);
     // enable lightings
     
-    blink(2, 150);
+    blink(2, 90, A_LED);
 
     Serial.println(WiFi.softAPIP());
     // enable light
-    blink(2, 30);
+    blink(2, 110, B_LED);
+
+    //mechanics init
+
 
     server.begin();
     server.on("/", handleIndex);
@@ -41,15 +48,18 @@ void loop() {
     server.handleClient();
 }
 
-void blink(int times, int interval) {
-    for(int i = 0; i < times; i++) {
-        digitalWrite(B_LED, LOW);
+void blink(int times, int interval, uint led) {
+
+    /* 
+        interval => time of turned ON
+    */
+    
+    delay(70);
+    for (int i = 0; i < times; i++) {
+        digitalWrite(led, LOW);
         delay(interval);
-        digitalWrite(B_LED, HIGH);
-        delay(90);
-        digitalWrite(B_LED, LOW);
-        delay(interval);
-        digitalWrite(B_LED, HIGH);
+        digitalWrite(led, HIGH);
+        delay(70);
     }
 }
 
